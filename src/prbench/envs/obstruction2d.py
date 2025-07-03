cdbab35db93549d4c70aaef0331b6a55dd03b607
Tom Silver <tomssilver@gmail.com>
2025-07-03 08:15:35 -0400

    wip

--- src/prbench/envs/obstruction2d.py
+++ src/prbench/envs/obstruction2d.py
@@ -1,8 +1,7 @@
 """Obstruction 2D env (more description coming soon)."""
 
-from geom2drobotenvs.envs.obstruction_2d_env import (
-    Obstruction2DEnv as Geom2DObstruction2DEnv,)
+from geom2drobotenvs.envs.obstruction_2d_env import Obstruction2DEnv as G2DOE
 
 
-class Obstruction2DEnv(Geom2DObstruction2DEnv):
+class Obstruction2DEnv(G2DOE):
     """Obstruction 2D env (more description coming soon)."""
