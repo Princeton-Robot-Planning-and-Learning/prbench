98cb2133e3f52d758f1d86b13096f1fd35d1ba5d
Tom Silver <tomssilver@gmail.com>
2025-08-06 10:53:50 -0400

    wip

--- tests/test_envs.py
+++ tests/test_envs.py
@@ -12,10 +12,12 @@ def test_env_make_and_check_env():
     Also calls gymnasium.utils.env_checker.check_env() to test API
     functions.
     """
+    print("Running test_env_make_and_check_env", flush=True)
     prbench.register_all_environments()
     env_ids = prbench.get_all_env_ids()
     assert len(env_ids) > 0
     for env_id in env_ids:
+        print("Starting env_id", env_id, flush=True)
         # We currently require all environments to have RGB rendering.
         env = prbench.make(env_id, render_mode="rgb_array")
         assert env.render_mode == "rgb_array"
