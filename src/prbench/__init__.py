a074888fb018b009c6f97541295b991e845fa9e7
Tom Silver <tomssilver@gmail.com>
2025-08-06 10:31:19 -0400

    confused why this worked before but not now...

--- src/prbench/__init__.py
+++ src/prbench/__init__.py
@@ -6,20 +6,20 @@ import sys
 import gymnasium
 from gymnasium.envs.registration import register
 
+# Detect headless mode (no DISPLAY) and set OSMesa if needed
+if not os.environ.get("DISPLAY"):
+    # TODO do this more rigorously
+    if sys.platform == "darwin":
+        os.environ["MUJOCO_GL"] = "glfw"
+        os.environ["PYOPENGL_PLATFORM"] = "glfw"
+    else:
+        os.environ["MUJOCO_GL"] = "osmesa"
+        os.environ["PYOPENGL_PLATFORM"] = "osmesa"
+
 
 def register_all_environments() -> None:
     """Add all benchmark environments to the gymnasium registry."""
     # NOTE: ids must start with "prbench/" to be properly registered.
-
-    # Detect headless mode (no DISPLAY) and set OSMesa if needed
-    if not os.environ.get("DISPLAY"):
-        # TODO do this more rigorously
-        if sys.platform == "darwin":
-            os.environ["MUJOCO_GL"] = "glfw"
-            os.environ["PYOPENGL_PLATFORM"] = "glfw"
-        else:
-            os.environ["MUJOCO_GL"] = "osmesa"
-            os.environ["PYOPENGL_PLATFORM"] = "osmesa"
 
     # Obstructions2D environment with different numbers of obstructions.
     num_obstructions = [0, 1, 2, 3, 4]
