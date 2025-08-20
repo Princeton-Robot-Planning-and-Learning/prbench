bfd387a0576532ed13d0dc24d2a19382875bbc61
Tom Silver <tomssilver@gmail.com>
2025-08-20 16:37:04 -0400

    wip

--- src/prbench/envs/geom3d/motion3d.py
+++ src/prbench/envs/geom3d/motion3d.py
@@ -3,18 +3,22 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-import gymnasium
-import numpy as np
 from typing import Any
 
+import gymnasium
+import numpy as np
+import pybullet as p
+from numpy.typing import NDArray
 from prpl_utils.spaces import FunctionalSpace
 from pybullet_helpers.camera import capture_image
-from pybullet_helpers.geometry import Pose, get_pose, multiply_poses, set_pose
+from pybullet_helpers.geometry import Pose, get_pose, set_pose
 from pybullet_helpers.gui import create_gui_connection
+from pybullet_helpers.inverse_kinematics import (
+    InverseKinematicsError,
+    inverse_kinematics,
+)
 from pybullet_helpers.joint import JointPositions
 from pybullet_helpers.robots import create_pybullet_robot
-import pybullet as p
-
 
 
 @dataclass(frozen=True)
@@ -25,14 +29,31 @@ class Motion3DEnvSpec:
     robot_name: str = "kinova-gen3"
     robot_base_pose: Pose = Pose.identity()
     initial_joints: JointPositions = field(
-        default_factory=lambda: [-4.3, -1.6, -4.8, -1.8, -1.4, -1.1, 1.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        default_factory=lambda: [
+            -4.3,
+            -1.6,
+            -4.8,
+            -1.8,
+            -1.4,
+            -1.1,
+            1.6,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
     )
     end_effector_viz_radius: float = 0.01
     end_effector_viz_color: tuple[float, float, float, float] = (1.0, 0.2, 0.2, 0.5)
+    max_action_mag: float = 0.05
 
     # Target.
     target_radius: float = 0.1
     target_color: tuple[float, float, float, float] = (1.0, 0.2, 0.2, 0.5)
+    target_lower_bound: tuple[float, float, float] = (0.0, 0.1, 0.0)
+    target_upper_bound: tuple[float, float, float] = (0.5, 0.8, 0.5)
 
     # For rendering.
     render_dpi: int = 300
@@ -63,10 +84,8 @@ class Motion3DState:
 @dataclass(frozen=True)
 class Motion3DAction:
     """An action for Motion3DEnv().
-    
-    NOTE: the environment enforces a limit on the magnitude of the deltas.
 
-    TODO do the above.
+    NOTE: the environment enforces a limit on the magnitude of the deltas.
     """
 
     delta_joints: JointPositions  # change in joint positions
@@ -78,10 +97,11 @@ class Motion3DEnv(gymnasium.Env[Motion3DState, Motion3DAction]):
     # Only RGB rendering is implemented.
     metadata = {"render_modes": ["rgb_array"]}
 
-    def __init__(self,
-                 spec: Motion3DEnvSpec = Motion3DEnvSpec(),
-                 use_gui: bool = False,
-                 ) -> None:
+    def __init__(
+        self,
+        spec: Motion3DEnvSpec = Motion3DEnvSpec(),
+        use_gui: bool = False,
+    ) -> None:
         self._spec = spec
 
         # Set up Gymnasium env fields.
@@ -90,8 +110,13 @@ class Motion3DEnv(gymnasium.Env[Motion3DState, Motion3DAction]):
             "render_fps": self._spec.render_fps,
         }
         self.render_mode = "rgb_array"
-        self.observation_space = FunctionalSpace(contains_fn=lambda o: isinstance(o, Motion3DState))
-        self.action_space = FunctionalSpace(contains_fn=lambda a: isinstance(a, Motion3DAction))
+        self.observation_space = FunctionalSpace(
+            contains_fn=lambda o: isinstance(o, Motion3DState)
+        )
+        self.action_space = FunctionalSpace(
+            contains_fn=lambda a: isinstance(a, Motion3DAction),
+            sample_fn=self._sample_action,
+        )
 
         # Create the PyBullet client.
         if use_gui:
@@ -138,11 +163,11 @@ class Motion3DEnv(gymnasium.Env[Motion3DState, Motion3DAction]):
         )
 
         # Create the body.
-        self.cylinder_id = p.createMultiBody(
+        self.target_id = p.createMultiBody(
             baseMass=0,
             baseCollisionShapeIndex=-1,
             baseVisualShapeIndex=visual_id,
-            basePosition=(0, 0, 0),  # TODO set in reset()
+            basePosition=(0, 0, 0),  # set in reset()
             baseOrientation=(0, 0, 0, 1),
             physicsClientId=self.physics_client_id,
         )
@@ -152,19 +177,68 @@ class Motion3DEnv(gymnasium.Env[Motion3DState, Motion3DAction]):
     ) -> tuple[Motion3DState, dict]:
         super().reset(seed=seed)
 
-        # Reset the robot.
-        self.robot.set_joints(self._spec.initial_joints)
+        # Reset the target. Sample and check reachability.
+        target_pose: Pose | None = None
+        for _ in range(100_000):
+            target_position = self.np_random.uniform(
+                self._spec.target_lower_bound, self._spec.target_upper_bound
+            )
+            target_pose = Pose(tuple(target_position))
+            try:
+                inverse_kinematics(self.robot, target_pose, validate=True)
+            except InverseKinematicsError:
+                continue
+            # If the goal is already reached, keep sampling.
+            if not self._goal_reached():
+                break
+        if target_pose is None:
+            raise RuntimeError("Failed to find reachable target position")
+        set_pose(self.target_id, target_pose, self.physics_client_id)
 
-        # TODO reset the target.
+        # Reset the robot.
+        self._set_robot_joints(self._spec.initial_joints)
 
         return self._get_obs(), {}
 
-    def step(self, action: Motion3DAction) -> tuple[Motion3DState, float, bool, bool, dict]:
-        # TODO
-        return self._get_obs(), 0.0, False, False, {}
-    
+    def step(
+        self, action: Motion3DAction
+    ) -> tuple[Motion3DState, float, bool, bool, dict]:
+        delta_joints = np.clip(
+            action.delta_joints, -self._spec.max_action_mag, self._spec.max_action_mag
+        )
+        current_joints = self.robot.get_joint_positions()
+        next_joints = np.clip(
+            current_joints + delta_joints,
+            self.robot.joint_lower_limits,
+            self.robot.joint_upper_limits,
+        ).tolist()
+        self.robot.set_joints(next_joints)
+        reward = -1
+        terminated = self._goal_reached()
+        return self._get_obs(), reward, terminated, False, {}
+
+    def render(self) -> NDArray[np.uint8]:  # type: ignore
+        return capture_image(self.physics_client_id, **self._spec.get_camera_kwargs())
 
     def _get_obs(self) -> Motion3DState:
         joint_positions = self.robot.get_joint_positions()
-        target = (0, 0, 0)  # TODO
+        target = get_pose(self.target_id, self.physics_client_id).position
         return Motion3DState(joint_positions, target)
+
+    def _goal_reached(self) -> bool:
+        target = get_pose(self.target_id, self.physics_client_id).position
+        end_effector_pose = self.robot.get_end_effector_pose()
+        dist = float(np.linalg.norm(np.subtract(target, end_effector_pose.position)))
+        return dist < self._spec.target_radius
+
+    def _set_robot_joints(self, joints: JointPositions) -> None:
+        self.robot.set_joints(joints)
+        end_effector_pose = self.robot.get_end_effector_pose()
+        set_pose(self.end_effector_viz_id, end_effector_pose, self.physics_client_id)
+
+    def _sample_action(self, rng: np.random.Generator) -> Motion3DAction:
+        num_dof = len(self.robot.get_joint_positions())
+        arr = rng.uniform(
+            -self._spec.max_action_mag, self._spec.max_action_mag, size=(num_dof,)
+        )
+        return Motion3DAction(arr.tolist())
--- tests/envs/geom3d/test_motion3d.py
+++ tests/envs/geom3d/test_motion3d.py
@@ -1,14 +1,21 @@
 """Tests for motion3d.py."""
 
-from prbench.envs.geom3d.motion3d import Motion3DEnv
+from prbench.envs.geom3d.motion3d import Motion3DAction, Motion3DEnv, Motion3DState
+
 
 def test_motion3d_env():
     """Tests for basic methods in motion3D env."""
-    
-    env = Motion3DEnv(use_gui=True)  # set use_gui=True to debug
-    obs, _ = env.reset()
+
+    env = Motion3DEnv(use_gui=False)  # set use_gui=True to debug
+    obs, _ = env.reset(seed=123)
+    assert isinstance(obs, Motion3DState)
+
+    for _ in range(10):
+        act = env.action_space.sample()
+        assert isinstance(act, Motion3DAction)
+        obs, _, _, _, _ = env.step(act)
 
     # Uncomment to debug.
-    import pybullet as p
-    while True:
-        p.getMouseEvents(env.physics_client_id)
+    # import pybullet as p
+    # while True:
+    #     p.getMouseEvents(env.physics_client_id)
