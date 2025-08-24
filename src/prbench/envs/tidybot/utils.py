22b1488ec70151f6d4d3bc0340a86fd8615d9373
Vaibhav Saxena <saxena.vaibhav96@gmail.com>
2025-08-24 12:28:30 -0400

    added utils for seeding

---
+++ src/prbench/envs/tidybot/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def get_rng(seed=None):
+    """Get a random number generator (RNG) based on the provided seed.
+
+    Args:
+        seed (int, optional): Seed for the RNG. If None, a default RNG is used.
+
+    Returns:
+        np.random.Generator: A NumPy random number generator.
+    """
+    if seed is not None:
+        assert isinstance(seed, int)
+        assert seed >= 0, "Seed must be a non-negative integer."
+
+        seed_seq = np.random.SeedSequence(entropy=seed)
+        rng = np.random.Generator(np.random.PCG64(seed_seq.entropy))
+        return rng
+    else:
+        return np.random.default_rng()
