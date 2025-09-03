f09ef615d5735ea91fdb2b4c07aaa4893d5caf18
Bowen Li <bowenli1024@gmail.com>
2025-09-03 16:37:38 -0400

    initial try of refactorization

--- scripts/basic_pymunk.py
+++ scripts/basic_pymunk.py
@@ -13,27 +13,117 @@ from pymunk.vec2d import Vec2d
 from prbench.envs.geom2d.structs import SE2Pose
 
 
-def create_gripper_base():
-    vs = [(-2, 25), (-2, -25), (2, -25), (2, 25)]
-    # mass = 1
-    # moment = pymunk.moment_for_poly(mass, vs)
-    gripper_base_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-
-    gripper_base_shape = pymunk.Poly(gripper_base_body, vs)
-    gripper_base_shape.friction = 1
-    gripper_base_shape.collision_type = 0
-    gripper_base_shape.density = 1.0
-    return gripper_base_body, gripper_base_shape
-
-def create_finger():
-    vs = [(-12, 2), (-12, -2), (12, -2), (12, 2)]
-    finger_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-
-    finger_shape = pymunk.Poly(finger_body, vs)
-    finger_shape.friction = 1
-    finger_shape.collision_type = 2
-    finger_shape.density = 1.0
-    return finger_body, finger_shape
+class KinRobot:
+    def __init__(self, base_radius=30, arm_length=32, gripper_gap_max=50):
+        self.base_radius = base_radius
+        self.arm_length = arm_length
+        self.gripper_gap_max = gripper_gap_max
+        self.gripper_gap = gripper_gap_max
+        self.position = Vec2d(100, 500)
+        self.angle = 0.0
+        self.speed = 2.5
+        self.gripper_speed = 1.0
+        
+        self.base_body = None
+        self.base_shape = None
+        self.gripper_base_body = None
+        self.gripper_base_shape = None
+        self.left_finger_body = None
+        self.left_finger_shape = None
+        self.right_finger_body = None
+        self.right_finger_shape = None
+        
+        self.create_components()
+    
+    def create_base(self):
+        self.base_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        self.base_shape = pymunk.Circle(self.base_body, self.base_radius)
+        self.base_shape.color = (255, 50, 50, 255)
+        self.base_body.position = self.position
+        return self.base_body, self.base_shape
+    
+    def create_gripper_base(self):
+        vs = [(-2, 25), (-2, -25), (2, -25), (2, 25)]
+        self.gripper_base_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        
+        self.gripper_base_shape = pymunk.Poly(self.gripper_base_body, vs)
+        self.gripper_base_shape.friction = 1
+        self.gripper_base_shape.collision_type = 0
+        self.gripper_base_shape.density = 1.0
+        return self.gripper_base_body, self.gripper_base_shape
+    
+    def create_finger(self):
+        vs = [(-12, 2), (-12, -2), (12, -2), (12, 2)]
+        finger_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        
+        finger_shape = pymunk.Poly(finger_body, vs)
+        finger_shape.friction = 1
+        finger_shape.collision_type = 2
+        finger_shape.density = 1.0
+        return finger_body, finger_shape
+    
+    def create_components(self):
+        self.create_base()
+        self.create_gripper_base()
+        self.left_finger_body, self.left_finger_shape = self.create_finger()
+        self.right_finger_body, self.right_finger_shape = self.create_finger()
+    
+    def add_to_space(self, space):
+        space.add(self.base_body, self.base_shape)
+        space.add(self.gripper_base_body, self.gripper_base_shape)
+        space.add(self.left_finger_body, self.left_finger_shape)
+        space.add(self.right_finger_body, self.right_finger_shape)
+    
+    def update(self, keys, mouse_pos, mouse_pressed):
+        if keys[pygame.K_w]:
+            self.position += Vec2d(0, -1) * self.speed
+        if keys[pygame.K_s]:
+            self.position += Vec2d(0, 1) * self.speed
+        if keys[pygame.K_a]:
+            self.position += Vec2d(-1, 0) * self.speed
+        if keys[pygame.K_d]:
+            self.position += Vec2d(1, 0) * self.speed
+        
+        self.angle = (mouse_pos - self.position).angle
+        
+        gripper_dy = 0.0
+        if mouse_pressed[0]:
+            gripper_dy = -self.gripper_speed
+        else:
+            gripper_dy = self.gripper_speed
+        
+        current_finger_pose = SE2Pose(
+            x=self.left_finger_body.position.x,
+            y=self.left_finger_body.position.y,
+            theta=self.left_finger_body.angle,
+        )
+        body_pose = SE2Pose(x=self.position.x, y=self.position.y, theta=self.angle)
+        current_relative_finger_pose = body_pose.inverse * current_finger_pose
+        relative_y = abs(min(current_relative_finger_pose.y + gripper_dy, self.gripper_gap_max // 2))
+        
+        self.update_positions(relative_y)
+    
+    def update_positions(self, finger_relative_y):
+        self.base_body.position = self.position
+        self.base_body.angle = self.angle
+        
+        body_pose = SE2Pose(x=self.position.x, y=self.position.y, theta=self.angle)
+        
+        relative_pose_gripper_base = SE2Pose(x=self.arm_length, y=0.0, theta=0.0)
+        gripper_base_pose = body_pose * relative_pose_gripper_base
+        self.gripper_base_body.position = (gripper_base_pose.x, gripper_base_pose.y)
+        self.gripper_base_body.angle = gripper_base_pose.theta
+        
+        new_relative_finger_pose_l = SE2Pose(x=45.0, y=finger_relative_y, theta=0.0)
+        new_relative_finger_pose_r = SE2Pose(x=45.0, y=-finger_relative_y, theta=0.0)
+        
+        l_finger_pose = body_pose * new_relative_finger_pose_l
+        self.left_finger_body.position = (l_finger_pose.x, l_finger_pose.y)
+        self.left_finger_body.angle = l_finger_pose.theta
+        
+        r_finger_pose = body_pose * new_relative_finger_pose_r
+        self.right_finger_body.position = (r_finger_pose.x, r_finger_pose.y)
+        self.right_finger_body.angle = r_finger_pose.theta
 
 width, height = 690, 600
 
@@ -74,24 +164,9 @@ def main():
     shape.density = 1.0
     space.add(b2, shape)
 
-    # "Cannon" that can fire arrows
-    cannon_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-    cannon_shape = pymunk.Circle(cannon_body, 30)
-    cannon_shape.color = (255, 50, 50, 255)
-    cannon_body.position = 100, 500
-    space.add(cannon_body, cannon_shape)
-
-    gripper_base_body, gripper_base_shape = create_gripper_base()
-    space.add(gripper_base_body, gripper_base_shape)
-    gripper_base_body.position = 132, 500
-
-    l_gripper_finger_body, l_gripper_finger_shape = create_finger()
-    space.add(l_gripper_finger_body, l_gripper_finger_shape)
-    l_gripper_finger_body.position = 145, 525
-
-    r_gripper_finger_body, r_gripper_finger_shape = create_finger()
-    space.add(r_gripper_finger_body, r_gripper_finger_shape)
-    r_gripper_finger_body.position = 145, 475
+    # Create robot
+    robot = KinRobot(base_radius=30, arm_length=32, gripper_gap_max=50)
+    robot.add_to_space(space)
 
     start_time = 0
     while running:
@@ -106,72 +181,12 @@ def main():
                 start_time = pygame.time.get_ticks()
 
         keys = pygame.key.get_pressed()
-
-        gripper_dy = 0.0
-        gripper_speed = 1.0
-        if pygame.mouse.get_pressed()[0]:
-            gripper_dy = -gripper_speed
-        else:
-            gripper_dy = gripper_speed
-
-        relative_pose_gripper_base = SE2Pose(
-            x=32.0, y=0.0, theta=0.0
-        )
-        curr_body_pose = SE2Pose(
-            x=cannon_body.position.x,
-            y=cannon_body.position.y,
-            theta=cannon_body.angle,
-        )
-        curr_finger_pose = SE2Pose(
-            x=l_gripper_finger_body.position.x,
-            y=l_gripper_finger_body.position.y,
-            theta=l_gripper_finger_body.angle,
-        )
-        curr_relative_finger_pose = curr_body_pose.inverse * curr_finger_pose
-        relative_y = abs(min(curr_relative_finger_pose.y + gripper_dy, 25.0))
-
-        new_relative_finger_pose_l = SE2Pose(
-            x=45.0,
-            y=relative_y,
-            theta=0.0,
-        )
-        new_relative_finger_pose_r = SE2Pose(
-            x=45.0,
-            y=-relative_y,
-            theta=0.0,
-        )
-
-        speed = 2.5
-        if keys[pygame.K_w]:
-            cannon_body.position += Vec2d(0, -1) * speed
-        if keys[pygame.K_s]:
-            cannon_body.position += Vec2d(0, 1) * speed
-        if keys[pygame.K_a]:
-            cannon_body.position += Vec2d(-1, 0) * speed
-        if keys[pygame.K_d]:
-            cannon_body.position += Vec2d(1, 0) * speed
-
         mouse_position = pymunk.pygame_util.from_pygame(
             Vec2d(*pygame.mouse.get_pos()), screen
         )
-        cannon_body.angle = (mouse_position - cannon_body.position).angle
-        # move the unfired arrow together with the cannon
-        body_pose = SE2Pose(
-            x=cannon_body.position.x,
-            y=cannon_body.position.y,
-            theta=cannon_body.angle,
-        )
-        gripper_base_pose = body_pose * relative_pose_gripper_base
-        gripper_base_body.position = (gripper_base_pose.x, gripper_base_pose.y)
-        gripper_base_body.angle = gripper_base_pose.theta
-
-        l_finger_pose = body_pose * new_relative_finger_pose_l
-        l_gripper_finger_body.position = (l_finger_pose.x, l_finger_pose.y)
-        l_gripper_finger_body.angle = l_finger_pose.theta
-
-        r_finger_pose = body_pose * new_relative_finger_pose_r
-        r_gripper_finger_body.position = (r_finger_pose.x, r_finger_pose.y)
-        r_gripper_finger_body.angle = r_finger_pose.theta
+        mouse_pressed = pygame.mouse.get_pressed()
+        
+        robot.update(keys, mouse_position, mouse_pressed)
 
         ### Clear screen
         screen.fill(pygame.Color("black"))
